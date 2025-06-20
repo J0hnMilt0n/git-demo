6ae80df7b50c4e7935b2bb04ef7bebc3a87438ec
Kavya <2003kavyabg@gmail.com>
2025-06-20 09:49:24 +0530

    hi

--- demo.js
+++ demo.js
@@ -2,18 +2,7 @@ console.log("Welcome to Github class")
 console.log("I've made one mistake")
 console.log("I'm developing a new feature")
 
-console.log("welcome")
-
-console.log("Hey!! Everyone Welcome to Git-hub Class")
-
-console.log("Hey!! Everyone Welcome to Git-hub Class")
 
 
-console.log("Hey!! there")
-console.log("pentagon")
-console.log("Hello my dear students of pentagon space!!")
-console.log("Hello my  students how are you!!")
-console.log('Hey')
-console.log('resolve merge conflict')
-console.log("Hello, I'm Rohit")
-console.log('The end of Beginning')
+
+console.log("Hey!! Everyone")
