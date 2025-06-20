90ba7a936c4372d08315f2b8ae298df161618e58
Soundarya-1890 <soundaryapavadshetty@gmail.com>
2025-06-20 09:28:09 +0530

    resolved conflicts

--- demo.js
+++ demo.js
@@ -1,13 +1,14 @@
 console.log("Welcome to Github class")
 console.log("I've made one mistake")
 console.log("I'm developing a new feature")
-console.log("im soundarya")
-
-
-console.log("Hello my dear sir")
 
 console.log("welcome")
 
 
 console.log("Hello my dear students!!")
 console.log("ola amigo")
+console.log("im soundarya")
+
+
+
+console.log("Hello my dear sir")
