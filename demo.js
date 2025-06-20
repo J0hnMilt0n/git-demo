91b4a9eb339fd7007c3d1f34130b7815611d2ad7
Rohit Khot <rohitkhot414@gmail.com>
2025-06-20 09:36:05 +0530

    Updated

--- demo.js
+++ demo.js
@@ -5,13 +5,5 @@ console.log("I'm developing a new feature")
 console.log("welcome")
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("Hey!! Everyone Welcome to Git-hub Class")
-=======
-console.log("Hello my dear students!!")
-console.log("ola amigo")
->>>>>>> adce99a8737174af5e81777004bcb1bcaea7a058
-=======
 console.log("Hello, I'm Rohit")
->>>>>>> 5d3590b47c7d50767c8d761571e5536350d7962c
+console.log('The end of Beginning')
